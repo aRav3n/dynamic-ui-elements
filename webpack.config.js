48383dc9c3cc34b8a32fb333e4d59789d08d93ef
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-11-04 14:28:13 -0600

    Set up webpack for css and favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.js',
+  //  mode: 'development',
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDo List",
+      favicon: "./src/images/favicon.png",
+    }),
+  ],
 };

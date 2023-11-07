36aab0cf610b32eec27fca69ef4978c4195c8548
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-11-07 11:31:16 -0700

    Change dropdown to procution mode

--- dropdown/webpack.config.js
+++ dropdown/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  //  mode: 'development',
-  mode: "development",
+  mode: "production",
+  /** mode: "development",
   devtool: "inline-source-map",
+  */
   entry: "./src/index.js",
   devtool: "inline-source-map",
   entry: "./src/index.js",

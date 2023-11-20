c6f015cead843f7c4b7510fdc5d42ba99a425e56
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-11-20 11:29:34 -0700

    Style images and add overlays for previous and next images
    Need to position arrows to be image overlays

---
+++ image-slider/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  // mode: "production",
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
